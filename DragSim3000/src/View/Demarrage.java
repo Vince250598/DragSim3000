bfa91a6f1488fa2bdbe5bde40e7e02d8352334cc
Guillaume Simard <guillaume.simard.sdec@gmail.com>
2017-02-16 12:06:01 -0500

    texte animer au début

--- DragSim3000/src/View/Demarrage.java
+++ DragSim3000/src/View/Demarrage.java
@@ -2,17 +2,28 @@ package View;
 
 import Controller.EventHandler;
 import Main.Programme;
+import javafx.animation.Animation;
+import javafx.animation.KeyValue;
+import javafx.animation.ScaleTransition;
+import javafx.animation.Timeline;
 import javafx.scene.Scene;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Demarrage {
 
     private Programme prog = new Programme();
     private Pane group = new Pane();
     private Scene start = new Scene(group, prog.getLargeurEcran(), prog.getHauteurEcran());
-    private Image background = new Image("\\Ressources\\heure.jpg");
+    private Image background = new Image("\\Ressources\\background.png");
 
     public Demarrage(Stage s, EventHandler eh) {
         demarrer(s, eh);
@@ -31,5 +42,27 @@ public class Demarrage {
         Background bg = new Background(bgImg);
         p.setBackground(bg);
         //TODO: ajouter le texte et autres trucs si nécessaire
+
+        Text txt = new Text("Press any key\n to continue");
+        txt.setFill(Color.RED);
+        txt.setFont(Font.font(null, FontWeight.BOLD, 100));
+        txt.setTranslateX(950);
+        txt.setTranslateY(350);
+        txt.setRotate(25);
+        GaussianBlur gb = new GaussianBlur();
+        DropShadow ds = new DropShadow();
+        ds.setOffsetX(18);
+        ds.setOffsetY(18);
+        gb.setInput(ds);
+        txt.setEffect(gb);
+        p.getChildren().add(txt);
+
+        ScaleTransition st = new ScaleTransition(Duration.millis(1000), txt);
+        st.setAutoReverse(true);
+        st.setCycleCount(Animation.INDEFINITE);
+        st.setByX(1.1);
+        st.setByY(1.1);
+        st.play();
+
     }
 }
