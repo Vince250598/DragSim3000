b4f156011bed8afa857e075636f92b1bc09cb6bd
Vincent Gagnon <vincentgagnon53@gmail.com>
2017-02-20 10:09:38 -0500

    Classe EnCourse

--- DragSim3000/src/Controller/Moteur.java
+++ DragSim3000/src/Controller/Moteur.java
@@ -4,6 +4,14 @@ import Model.Voiture;
 
 public class Moteur {
     private static Voiture choixVoiture;
+    private double accelX;
+
+    public Moteur(){}
+
+   // public double calculVitesseX(){}
+
+
+
 
     public static Voiture getChoixVoiture() {
         return choixVoiture;
---
+++ DragSim3000/src/View/EnCourse.java
@@ -0,0 +1,62 @@
+package View;
+
+
+import Controller.Moteur;
+import Main.Programme;
+import Model.ListeVoitures;
+import Model.Voiture;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+public class EnCourse {
+
+    private Programme programme = new Programme();
+    ListeVoitures lv = new ListeVoitures();
+    private Pane group = new Pane();
+    private Scene enCourse = new Scene(group, programme.getLargeurEcran(), programme.getHauteurEcran());
+    private Image background = new Image("Ressources\\background.png");
+    //TODO:changer background
+    private Voiture v = lv.getVoitures().get(0);
+    //TODO: changer voiture pour Moteur.choixVoiture
+    private ImageView voiture = v.getImage();
+
+    public EnCourse() {
+        addElements(group);
+    }
+
+    private void addElements(Pane p) {
+        BackgroundImage backgroundImage = new BackgroundImage(background, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                new BackgroundSize(300, 800, false, false, false, false));
+
+        VBox vb = new VBox(20);
+        Label temps = new Label("Temps: ");
+        temps.setFont(Font.font(null, FontWeight.SEMI_BOLD, 15));
+        Label vitesse = new Label("Vitesse: ");
+        vitesse.setFont(Font.font(null, FontWeight.SEMI_BOLD, 15));
+        Label RPM = new Label("RPM: ");
+        RPM.setFont(Font.font(null, FontWeight.SEMI_BOLD, 15));
+        Label distance = new Label("Distance: ");
+        distance.setFont(Font.font(null, FontWeight.SEMI_BOLD, 15));
+        vb.getChildren().addAll(temps, vitesse, RPM, distance);
+        voiture.setY(520);
+        voiture.setX(-60);
+        voiture.setScaleX(0.2);
+        voiture.setScaleY(0.2);
+
+        Background bg = new Background(backgroundImage);
+        p.setBackground(bg);
+
+        p.getChildren().addAll(vb, voiture);
+
+
+    }
+
+    public Scene getEnCourse() {
+        return enCourse;
+    }
+}
